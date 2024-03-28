dbb78db888f8104c878038eec4e80beca3e13c96
Aurelio Chagas <aurelio.chagas.c@gmail.com>
2024-03-28 16:08:14 -0300

    style: creation and styling of the header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,20 @@
+import { MdShoppingBasket } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import { CarLink, Container } from './styles'
 export function Header() {
-  return <div>Header</div>
+  return (
+    <Container>
+      <Link to="/">
+        <h1>WeMovies</h1>
+      </Link>
+
+      <CarLink to="/">
+        <div>
+          <p>Meu Carrrinho</p>
+          <span>0 itens</span>
+        </div>
+        <MdShoppingBasket size={24} />
+      </CarLink>
+    </Container>
+  )
 }
---
+++ src/components/header/styles.ts
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  width: 100%;
+
+  padding: 2rem 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+`
+
+export const CarLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: right;
+
+    p,
+    span {
+      font-weight: 600;
+      transition: all 0.2s ease-in-out;
+      line-height: 1.1;
+    }
+
+    p {
+      font-size: 0.875rem;
+    }
+
+    span {
+      font-size: 0.75rem;
+      color: ${({ theme }) => theme['grey-500']};
+    }
+  }
+
+  @media (max-width: 430px) {
+    div {
+      p {
+        display: none;
+      }
+    }
+  }
+`
